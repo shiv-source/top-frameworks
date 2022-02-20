26f50b063262663337a28388cf519104b6e43132
shiv <shivk7466@gmail.com>
2022-02-21 02:25:04 +0530

    code uploaded

---
+++ main.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
+
+var client *http.Client
+
+type Repo struct {
+	Name           string `json:"name"`
+	Stars          int    `json:"stargazers_count"`
+	Forks          int    `json:"forks_count"`
+	Issues         int    `json:"open_issues_count"`
+	URL            string `json:"html_url"`
+	Description    string `json:"description"`
+	Private        bool   `json:"private"`
+	DefaultBranch  string `json:"default_branch"`
+	CommitsUrl     string `json:"commits_url"`
+	Language       string `json:"language"`
+	LastCommitDate string
+}
+
+type HeadCommit struct {
+	Sha    string `json:"sha"`
+	Commit struct {
+		Committer struct {
+			Name  string    `json:"name"`
+			Email string    `json:"email"`
+			Date  time.Time `json:"date"`
+		} `json:"committer"`
+	} `json:"commit"`
+}
+
+func main() {
+	godotenv.Load()
+	fileName := "project-list.txt"
+	urls := loadProjects(fileName)
+	githubUrl := "https://github.com/"
+	github_access_token := os.Getenv("GITHUB_ACCESS_TOKEN")
+
+	projects := []Repo{}
+
+	for _, url := range urls {
+
+		if strings.HasPrefix(url, githubUrl) {
+
+			repoFullName := strings.TrimPrefix(url, githubUrl)
+			fmt.Println(repoFullName)
+
+			repoUrl := fmt.Sprintf("https://api.github.com/repos/%s", repoFullName)
+			commitUrl := fmt.Sprintf("https://api.github.com/repos/%s/commits", repoFullName)
+
+			fmt.Println(repoUrl)
+
+			project := getRepoInfo(repoUrl, github_access_token, commitUrl)
+
+			projects = append(projects, project)
+		}
+	}
+
+	sort.Slice(projects, func(i, j int) bool {
+		return projects[i].Stars > projects[j].Stars
+	})
+
+	saveToReadme(projects, "readme.md")
+
+	fmt.Println("Total Projects: =", len(urls))
+}
+
+func loadProjects(fileName string) []string {
+
+	byteCodes, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	projects := strings.Split(string(byteCodes), "\n")
+
+	return projects
+}
+
+func getData(url string, token string, target interface{}) error {
+
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("Authorization", token)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client = &http.Client{Timeout: 10 * time.Second}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(target)
+}
+
+func getCommitInfo(commitUrl string, token string) string {
+
+	var commit []HeadCommit
+
+	err := getData(commitUrl, token, &commit)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return commit[0].Commit.Committer.Date.Format("2006-01-02 15:04:05")
+}
+
+func getRepoInfo(repoUrl string, github_access_token string, commitUrl string) Repo {
+
+	var repo Repo
+
+	err := getData(repoUrl, github_access_token, &repo)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	repo.LastCommitDate = getCommitInfo(commitUrl, github_access_token)
+
+	return repo
+}
+
+func saveToReadme(repos []Repo, fileName string) {
+
+	header := `# Top Frameworks
+## A list of top frameworks ranked by stars on github.  
+Please update the project-list.txt file.
+
+| SL| Name  | Stars| Forks| Issues | Language | Description | Last Commit |
+| --| ------| -----| ---- | ------ | -------- | ----------- | ----------- |
+`
+
+	readme, err := os.OpenFile(fileName, os.O_RDWR|os.O_TRUNC|os.O_CREATE, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer readme.Close()
+
+	readme.WriteString(header)
+
+	for i, repo := range repos {
+
+		description := strings.ReplaceAll(repo.Description, "\n", "")
+
+		readme.WriteString(fmt.Sprintf("| %d | [%s](%s) | %d | %d | %d | %s | %s | %s |\n", i+1, repo.Name, repo.URL, repo.Stars, repo.Forks, repo.Issues, repo.Language, description, repo.LastCommitDate))
+	}
+
+	fmt.Println("Successfully written the Markdown file")
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Top Frameworks
+## A list of top frameworks ranked by stars on github.  
+Please update the project-list.txt file.
+
+| SL| Name  | Stars| Forks| Issues | Language | Description | Last Commit |
+| --| ------| -----| ---- | ------ | -------- | ----------- | ----------- |
+| 1 | [angular](https://github.com/angular/angular) | 79713 | 20968 | 1734 | TypeScript | The modern web developer’s platform | 2022-02-18 21:57:03 |
+| 2 | [django](https://github.com/django/django) | 62457 | 26601 | 173 | Python | The Web framework for perfectionists with deadlines. | 2022-02-19 19:36:01 |
+| 3 | [flask](https://github.com/pallets/flask) | 58017 | 14865 | 12 | Python | The Python micro framework for building web applications. | 2022-02-19 15:12:40 |
+| 4 | [gin](https://github.com/gin-gonic/gin) | 55727 | 6307 | 484 | Go | Gin is a HTTP web framework written in Go (Golang). It features a Martini-like API with much better performance -- up to 40 times faster. If you need smashing performance, get yourself some Gin. | 2022-02-14 06:39:57 |
+| 5 | [beego](https://github.com/beego/beego) | 27750 | 5415 | 27 | Go | beego is an open-source, high-performance web framework for the Go programming language. | 2022-02-06 15:12:12 |
